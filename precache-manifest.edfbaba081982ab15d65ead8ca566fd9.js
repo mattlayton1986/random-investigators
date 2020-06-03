1397c7d1c33fa7c350501e48194babbb2e6c7009
Matt Layton <mattlayton@Matts-MBP.cable.rcn.com>
2020-06-02 19:09:26 -0500

    Updates

---
+++ precache-manifest.edfbaba081982ab15d65ead8ca566fd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6148cdeb7a8129c552de36f9194f5f30",
+    "url": "/random-investigators/index.html"
+  },
+  {
+    "revision": "8061736aeae04734b24b",
+    "url": "/random-investigators/static/css/main.57b7373d.chunk.css"
+  },
+  {
+    "revision": "5d3ba474d87d41e42ecd",
+    "url": "/random-investigators/static/js/2.a9fa88a9.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/random-investigators/static/js/2.a9fa88a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8061736aeae04734b24b",
+    "url": "/random-investigators/static/js/main.b1dd5e7c.chunk.js"
+  },
+  {
+    "revision": "68c316b2d4707e74b281",
+    "url": "/random-investigators/static/js/runtime-main.27e924d7.js"
+  }
+]);
